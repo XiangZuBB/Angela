45543186d46f6338a88c7452542532fecccb82b8
XiangZuBB <126643276+XiangZuBB@users.noreply.github.com>
2023-07-13 23:48:21 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
@@ -33,10 +32,10 @@ var config = {
         turn_on: "开始",
         play: "音乐",
         bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        balloons_flying: "这是一个礼品",
+        cake_fadein: "蛋糕",
+        light_candle: "烛火",
+        wish_message: "哈哈哈哈没想到吧是个赛博礼品",
         story: "A MESSAGE FOR YOU",
     }
 };
